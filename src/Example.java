58167d43f60123d62dc696d7e15109b207433862
Katarzyna Norys <katanor457@student.polsl.pl>
2021-06-03 13:08:34 +0200

    Changes for Example class.

---
+++ src/Example.java
@@ -0,0 +1,8 @@
+public class Example
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Umiesc mnie w githubie");
+        System.out.println("Umiesc mnie w githubie v2");
+    }
+}
